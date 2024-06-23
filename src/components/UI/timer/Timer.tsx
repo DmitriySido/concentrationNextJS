0cc99a0bcc461abacc2cbe1f2fe313a10870c770
Dmitriy <dimakrytoytshel@gmail.com>
2024-06-23 15:21:46 +0300

    addStyledTimer

--- src/components/UI/timer/Timer.tsx
+++ src/components/UI/timer/Timer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import './timer.css'
+import './timer.css'
 import PopupResults from '../popups/popupResults/PopupResults';
 
 interface ITimerProps {
