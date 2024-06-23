92974af816b7e92176fd5418e63aa8aae8284463
Dmitriy <dimakrytoytshel@gmail.com>
2024-06-23 15:03:09 +0300

    fixed timer

--- src/components/UI/timer/Timer.tsx
+++ src/components/UI/timer/Timer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Timer.scss'
+import './timer.scss'
 import PopupResults from '../popups/popupResults/PopupResults';
 
 interface ITimerProps {
