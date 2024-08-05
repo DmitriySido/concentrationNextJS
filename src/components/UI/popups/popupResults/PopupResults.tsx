832238dccbd7c1304c52e44d6e48065d8b0b54a7
Dmitriy <dimakrytoytshel@gmail.com>
2024-08-05 15:37:12 +0300

    add edits to project

--- src/components/UI/popups/popupResults/PopupResults.tsx
+++ src/components/UI/popups/popupResults/PopupResults.tsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react"
 import ToggleButton from "../../buttons/buttonStart/ToggleButton"
 import './PopupResults.scss'
+import popupResultsLogic from "./popupResultsLogic";
+
 
 interface IResults {
   wrongAnswers: number,
   rightAnswers: number,
 }
 
-const PopupResults = ({ wrongAnswers, rightAnswers } : IResults) => {
+export interface IUserData {
+  correctAnswers: number,
+  wrongAnswers: number,
+  totalTime: number,
+  passed: number,
+  largerNumber: number,
+  extraPiece: number,
+  numberOfSquares: number,
+  restoreSequence: number,
+};
 
-  const closePopup = () => false
+const PopupResults = ({ wrongAnswers, rightAnswers } : IResults) => {
+  const [userData, setUserData] = useState<IUserData | null>(null);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('userData')
+    storedData && setUserData(JSON.parse(storedData))
+  }, []);
+
+  useEffect(() => {
+    userData && popupResultsLogic({ rightAnswers, wrongAnswers, userData, setUserData })
+  }, [userData === null]);
+
+  const closePopup = () => false;
 
   return (
     <div className='popup-overlay' onClick={closePopup}>
@@ -16,10 +40,10 @@ const PopupResults = ({ wrongAnswers, rightAnswers } : IResults) => {
         <h2 className="popup-tilte">Ваши результаты: </h2>
         <h3 className="right-answer">Правильных ответов: <strong>{rightAnswers}</strong></h3>
         <h3 className="wrong-answer">Неправильных ответов: <strong>{wrongAnswers}</strong></h3>
-        <ToggleButton buttonClass='in-popup' path='/' tilte='Выйти'/>
+        <ToggleButton buttonClass='in-popup' path='/tasks' tilte='Выйти'/>
       </div>
     </div>
   )
 }
 
-export default PopupResults
+export default PopupResults;
